ac21a1393e27ef83e090b941ce57975552f11828
PELMEN9294 <aleks.lecha@gmail.com>
2016-06-28 17:10:14 +0300

    1

---
+++ Django/votes/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Questions(models.Model):
+    quesrion_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+
+class Choise(models.Model):
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    choise_text = models.CharField(max_length=200)
+    choise_date = models.DateTimeField('date choise')
