8dede9de87224afab70dc815146ad3c9ed3bd939
alex <aleks.lecha@gmail.com>
2016-07-02 21:38:22 +0300

    1

--- Django/votes/views.py
+++ Django/votes/views.py
@@ -1,17 +1,21 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from .models import Questions
 
 def index(request):
     latest_question_list = Questions.objects.order_by('-pub_date')[:5]
-    output = '. '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list':latest_question_list}
+    return render(request,'votes/index.html', context)
+
 
 def votes(request):
     return HttpResponse("You're voting on question %s." % question_id)
 
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
+
 
 def results(request, question_id):
     return HttpResponse("You're looking at the results of question %s." % question_id)
