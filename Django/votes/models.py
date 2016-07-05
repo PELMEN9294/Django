2c652ab65f56a7c02343e075e9f186b4ea375cf9
pelmen9294 <aleks.lecha@gmail.com>
2016-07-05 13:03:54 +0300

    1

--- Django/votes/models.py
+++ Django/votes/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Questions(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    test = models.
 
 
 
